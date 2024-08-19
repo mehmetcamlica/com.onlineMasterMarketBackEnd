96be20453c01483e7ce5406e0c009fabc60e993c
Selcuk <selcukemnn@gmail.com>
2024-08-19 21:34:33 +0300

    selcuk: add post for staff

--- src/test/java/stepdefinitions/API/StaffStepDefinitions.java
+++ src/test/java/stepdefinitions/API/StaffStepDefinitions.java
@@ -1,7 +1,13 @@
 package stepdefinitions.API;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonParser;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.DataTableType;
+import io.cucumber.java.DocStringType;
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.Given;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -15,19 +21,34 @@ import org.junit.Assert;
 import utilities.API_Utilities.APIReader;
 import utilities.API_Utilities.Authentication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class StaffStepDefinitions {
 
+	private static ObjectMapper objectMapper = new ObjectMapper();
 	Response response;
 	RequestSpecification spec;
 	String token;
 	String fullPath;
-
 	JSONObject expectedDataJSObject;
-
 	JsonPath responseJsonPath;
+	int addedId;
+
+	@Given("The api user close the api connection.Selcuk")
+	public void the_api_user_close_the_api_connection_selcuk() {
+		response = null;
+		spec = null;
+		fullPath = null;
+		token = null;
+		responseJsonPath = null;
+		addedId = 0;
+	}
 
 	@Given("The api user sets up spec for base url and valid token.Selcuk")
 	public void the_api_user_sets_up_spec_for_base_url_and_valid_token() {
@@ -84,14 +105,6 @@ public class StaffStepDefinitions {
 	@Given("The api user verifies that the {string} information in the response body is {string}.Selcuk")
 	public void the_api_user_verifies_that_the_information_in_the_response_body_is_selcuk(String path, String expectedResponse) {
 		response.then().body(path, Matchers.equalTo(expectedResponse));
-	}
-
-	@Given("The api user close the api connection.Selcuk")
-	public void the_api_user_close_the_api_connection_selcuk() {
-		response = null;
-		spec = null;
-		fullPath = null;
-		token = null;
 	}
 
 	@Given("The api user verifies that the data in the response body index[{int}] includes {int}, {int}, {string}, {string}, {string}, {string}.Selcuk")
@@ -170,5 +183,80 @@ public class StaffStepDefinitions {
 			Assert.assertEquals(each.get("status"), responseJsonPath.getString("data[" + i + "].status"));
 			i++;
 		}
+	}
+
+	@ParameterType("[^\"]*")
+	public List<String> post(String requestBodyForPost)
+	{
+		return (Arrays.asList(requestBodyForPost.split(",")));
+	}
+
+
+	@Given("The api user sends a post request with {post} .Selcuk")
+	public void the_api_user_sends_a_post_request_with_mac_edmont_mrtin_gmail_com_male_about_martin_selcuk(List<String> post) {
+		System.out.println(post);
+		response = RestAssured
+				.given()
+				.spec(spec)
+				.contentType(ContentType.JSON)
+				.when()
+				.body("")
+				.post(fullPath);
+	}
+
+	@DocStringType
+	public JsonNode json(String docString) throws JsonProcessingException {
+		return (objectMapper.readTree(docString));
+	}
+
+	@Given("The api user sends a post request with the body.Selcuk")
+	public void the_api_user_sends_a_post_request_with_the_body(JsonNode jsonNode) {
+		response = RestAssured
+				.given()
+				.spec(spec)
+				.contentType(ContentType.JSON)
+				.when()
+				.body(jsonNode.toString()).log().all()
+				.post(fullPath);
+		response.then().log().all();
+		addedId = response.jsonPath().getInt("data.added_staff_id");
+		System.out.println(addedId);
+	}
+
+	@Given("The api user deletes the post request.Selcuk")
+	public void the_api_user_deletes_the_post_request_selcuk() {
+		the_api_user_sets_path_parameters_selcuk("api/deleteStaff/" + addedId);
+		response = RestAssured
+				.given()
+				.spec(spec)
+				.contentType(ContentType.JSON)
+				.when().log().all()
+				.delete(fullPath);
+
+		response.then().log().all();
+	}
+
+	@Given("The api user send request from {string} file.Selcuk")
+	public void the_api_user_send_request_from_file_selcuk(String fileName) {
+		JSONObject requestBoydJSObject = getJsonObjectFromFile(fileName);
+		response = RestAssured
+				.given()
+				.spec(spec)
+				.contentType(ContentType.JSON)
+				.when()
+				.body(requestBoydJSObject.toString())
+				.post(fullPath);
+	}
+
+	public JSONObject getJsonObjectFromFile(String fileName) {
+		File file = new File("data/dataAPI/" + fileName);
+		JSONObject jsonObject = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+			jsonObject = new JSONObject(content);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return (jsonObject);
 	}
 }
