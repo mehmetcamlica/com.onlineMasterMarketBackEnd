adde7ed6b4c105f9bc48a1da37ac102c867a277f
toprak <toprakyemelek1995@gmail.com>
2024-08-21 22:41:32 +0300

    v4

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = {"src/test/resources/features/API", "src/test/resources/features/DB"},
         glue = {"stepdefinitions","hooks","utilities"},
-        tags = "@Damra",
+        tags = "@DAMRA",
         dryRun =false
 )
 public class Runner {
